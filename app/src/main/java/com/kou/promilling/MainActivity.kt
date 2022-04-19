8416351fe1e90ed0fe5b71f6cefe3917d9e3581c
kou-hc <konstantin0780@gmail.com>
2022-04-19 16:37:31 +0300

    Added navigation to calculators

--- app/src/main/java/com/kou/promilling/MainActivity.kt
+++ app/src/main/java/com/kou/promilling/MainActivity.kt
@@ -2,10 +2,36 @@ package com.kou.promilling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.kou.promilling.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupNavigation()
+    }
+
+    override fun onSupportNavigateUp() =
+        navigateUp(findNavController(R.id.nav_host_fragment), binding.drawerLayout)
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, binding.drawerLayout)
+
+        binding.navigationView.setupWithNavController(navController)
+
     }
 }
