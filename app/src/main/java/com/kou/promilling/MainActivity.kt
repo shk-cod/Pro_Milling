2a05c9feb5065a0af957c3a983ff4e3601d03068
kou-hc <konstantin0780@gmail.com>
2022-07-01 19:19:42 +0300

    Project prepared for release

--- app/src/main/java/com/kou/promilling/MainActivity.kt
+++ app/src/main/java/com/kou/promilling/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -23,7 +25,8 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setupNavigation() {
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
         setSupportActionBar(binding.toolbar)
         val appBarConfiguration = AppBarConfiguration(
