774842568a499336b1ff06eb53d02fe9d1b55259
shk-cod <konstantin0780@gmail.com>
2022-11-17 22:28:51 +0300

    Added espresso test (cutting width user flow test)

--- app/src/androidTest/java/com/kou/promilling/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kou/promilling/ExampleInstrumentedTest.kt
@@ -1,24 +1,42 @@
 package com.kou.promilling
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.filters.LargeTest
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+@LargeTest
+class ExampleEspressoTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kou.promilling", appContext.packageName)
+    fun cuttingWidthFlowTest() {
+        onView(withId(R.id.cutting_width))
+            .perform(click())
+
+        onView(withId(R.id.text_input_radius))
+            .perform(typeText("5"))
+        onView(withId(R.id.text_input_rounding_radius))
+            .perform(typeText("10"))
+        onView(withId(R.id.text_input_cutting_width))
+            .perform(typeText("15"))
+
+        onView(withId(R.id.button_result))
+            .perform(click())
+
+        onView(withId(R.id.text_view_result))
+            .check(matches(withText("")))
     }
 }
