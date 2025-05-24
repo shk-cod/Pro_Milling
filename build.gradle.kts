070bf44d52712640f0e1d0f66d854623c825ddd9
konstantin <konstantin0780@gmail.com>
2025-05-24 12:12:34 +0300

    AGP upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.8.2" apply false
-    id("com.android.library") version "8.8.2" apply false
+    id("com.android.application") version "8.10.0" apply false
+    id("com.android.library") version "8.10.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
     id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
