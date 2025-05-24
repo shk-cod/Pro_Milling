4de4542b31b4702e3e4367434e4a3edb9922dfae
konstantin <konstantin0780@gmail.com>
2025-05-24 11:46:40 +0300

    Dependencies upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ val versionPatch = 1
 
 android {
     namespace = "com.kou.promilling"
-    compileSdk = 34
+    compileSdk = 35
 
     buildFeatures {
         dataBinding = true
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.kou.promilling"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = versionMajor * 10000 + versionMinor * 100 + versionPatch
         versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
 
@@ -63,16 +63,16 @@ android {
 dependencies {
     // Androidx libraries
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.room:room-runtime:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.2")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.room:room-runtime:2.7.1")
+    ksp("androidx.room:room-compiler:2.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 
     // Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
@@ -86,7 +86,7 @@ dependencies {
 
     //Hilt
     implementation("com.google.dagger:hilt-android:2.51")
-    ksp("com.google.dagger:hilt-compiler:2.51")
+    ksp("com.google.dagger:hilt-compiler:2.56.2")
 
     // Tests
     testImplementation("junit:junit:4.13.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,16 @@ buildscript {
     }
 
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.2")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.0")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("com.android.library") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
+    id("com.android.application") version "8.8.2" apply false
+    id("com.android.library") version "8.8.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
 }
 
 
